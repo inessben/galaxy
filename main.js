2585f836a8b9e959b3250f1c7c39f5093c5cc319
inessben <ine.benss@gmail.com>
2023-06-11 18:12:37 +0200

    project in finishing

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ gltfLoader.load(
     (gltf) => {
         astronaut = gltf.scene
         astronaut.position.x = - 1.3
-        astronaut.position.y = -10
+        astronaut.position.y = -15
         astronaut.position.z = -0.5
 
         astronaut.scale.set(0.5, 0.5, 0.5)
