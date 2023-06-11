a23b89a849a07567f79f729534db7e98d07e778b
inessben <ine.benss@gmail.com>
2023-06-11 18:01:01 +0200

    project in finishing

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ gltfLoader.load(
     (gltf) => {
         astronaut = gltf.scene
         astronaut.position.x = - 1.3
-        astronaut.position.y = -5
+        astronaut.position.y = -10
 
         astronaut.scale.set(0.5, 0.5, 0.5)
 
@@ -145,8 +145,8 @@ scene.add(earth)
 
 // Lights
 // add an orange ambient light
-// const ambientLight = new THREE.AmbientLight(0xFF8900, 0.4)
-// scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0x888888, 0.4)
+scene.add(ambientLight)
 
 // add a directionnal light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5)
